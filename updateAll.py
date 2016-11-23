84f340441432d592b7dcd64c3ac61a82fd559d2f
Abram Wiebe <awiebe@burningthumb.com>
2016-11-22 23:05:01 -0800

    implemented update

---
+++ updateAll.py
@@ -0,0 +1,16 @@
+import sys
+import os
+from os import listdir
+
+
+
+def get_script_path():
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
+
+dataDir = os.path.join(get_script_path(),"data")
+onlyfiles = [f for f in listdir(dataDir) if os.path.isdir(os.path.join(dataDir, f)) ]
+onlyfiles = filter(lambda (x): not os.path.basename(x).startswith("."),onlyfiles)
+
+for f in onlyfiles:
+    sys.argv = ['tumblrScrape.py',f]
+    execfile('tumblrScrape.py')
